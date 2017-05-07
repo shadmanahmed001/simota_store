7e88e7d112ef038af6b97906dc91d57f75568884
Shadman Ahmed <shadman.ahmed001@gmail.com>
2017-05-07 11:42:44 -0700

    changed things to the login controller side

--- client/assets/controllers/loginController.js
+++ client/assets/controllers/loginController.js
@@ -5,19 +5,54 @@ app.controller('loginController', ['$scope', '$location', 'userFactory', '$cooki
   $scope.existingUser = {};
   $scope.error_messages = [];
 
+  // Google Sign In Functions
+  var onFailure = function(error) {
+    console.log(error);
+  };
+  // function onSuccess(googleUser) {
+  //   console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+  // };
+//   function renderButton() {
+//   gapi.signin2.render('my-signin2', {
+//   'scope': 'profile email',
+//   'width': 240,
+//   'height': 50,
+//   'longtitle': true,
+//   'theme': 'dark',
+//   'onsuccess': onSuccess,
+//   'onfailure': onFailure
+// });
+// }
+function onSignIn(googleUser) {
+  $scope.profile = googleUser.getBasicProfile();
+  console.log("in this funciton");
+  console.log('ID: ' + profile.getId());
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+};
+// Sign Out Of Google
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+
+  console.log($scope.profile);
 
   // -------------------------------------------------------------------------
   //                            Check Logged In User
   // -------------------------------------------------------------------------
-  var CheckingUser = function () {
-    if (!$cookieStore.get('logged-in')) {
-      console.log("Not Logged In");
-      // $location.url('/');
-    } else {
-      $location.url('/userdashboard');
-    }
-  };
-  CheckingUser();
+  // var CheckingUser = function () {
+  //   if (!$cookieStore.get('logged-in')) {
+  //     console.log("Not Logged In");
+  //     // $location.url('/');
+  //   } else {
+  //     $location.url('/userdashboard');
+  //   }
+  // };
+  // CheckingUser();
 
   // -------------------------------------------------------------------------
   //                            Login User
