61dfa5682ea88f7598a3a77c3d300885cec18ce6
Shadman Ahmed <shadman.ahmed001@gmail.com>
2017-06-01 13:20:36 -0700

    doing the bottom badge

--- client/assets/controllers/allProductsController.js
+++ client/assets/controllers/allProductsController.js
@@ -18,23 +18,11 @@ app.controller('allProductsController', ['$scope','productsFactory','$routeParam
 
 $scope.username = $cookieStore.get('username')
 
-
+$scope.quantityLeft = "Only 3 left"
 
   productsFactory.index(function(data) {
   $scope.products = data;
   })
-
-$scope.addToCart = function(product) {
-
-  // console.log("need to finish the cart stuff");
-  // console.log(product);
-  var email = $cookieStore.get('email');
-  productsFactory.addToCart({"email": email, "product": product}, function(data) {
-    console.log(data);
-    // DO SOMETHING WITH returned_data
-})
-}
-
 
   $scope.create = function() {
       productsFactory.create($scope.newProduct, function(data) {
@@ -54,14 +42,51 @@ $scope.addToCart = function(product) {
     });
   }
 
+// Add to Cart
+$scope.addToCart = function(product, $countVal) {
+  var email = $cookieStore.get('email');
+  $scope.countVal = 1
+  $("#add" + product._id).hide(function() {
+    $(".buttonchange" + product._id).show();
+  });
+  productsFactory.addToCart({"email": email, "product": product, "quantity": $scope.countVal}, function(data) {
+    console.log('this is the cart', data);
+    // $location.path('/all')
+  })
+}
+
+$scope.add = function(product) {
+  var email = $cookieStore.get('email');
+  if ($scope.countVal === product.quantity) {
+    console.log('item sold out');
+    return
+  }
+  $scope.countVal = $scope.countVal + 1
+  productsFactory.addToCart({"email": email, "product": product, "quantity": $scope.countVal}, function(data) {
+    console.log('this is the cart', data);
+    // $location.path('/all')
+  })
+}
+
+$scope.minus = function(product) {
+  var email = $cookieStore.get('email');
+  if ($scope.countVal <= 1){
+    console.log('Need min quantity');
+    return
+  }
+  $scope.countVal = $scope.countVal - 1
+  productsFactory.addToCart({"email": email, "product": product, "quantity": $scope.countVal}, function(data) {
+    console.log('this is the cart', data);
+    // $location.path('/all')
+  })
+}
+
 // jQuery
 $(".button-collapse").sideNav();
+
 $(".searchToggle").click(function() {
   $("#searchDiv").slideToggle( "slow" );
 });
-
-
-
 
 
 
